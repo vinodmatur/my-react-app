6ed62cc8d24e25070b08b1d3a9580cbc7afa7d6f
vinodmatur <34124715+vinodmatur@users.noreply.github.com>
2021-12-27 18:02:27 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import Table from './table'
+import Form from './form'
+class App extends Component {
+    state = {
+        characters: [],
+    }
+    render(){
+        const { characters } = this.state
+        return (
+            <div className="container">
+              <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+              <Form handleSubmit={this.handleSubmit}/>
+            </div>
+          )
+    }
+
+    removeCharacter = (index) => {
+        const {characters} = this.state
+        this.setState({
+            characters: characters.filter((character, i) => {
+              return i !== index
+            }),
+          })
+    }
+
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
+}
+  export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+import Api from './api'
+import './index.css'
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
